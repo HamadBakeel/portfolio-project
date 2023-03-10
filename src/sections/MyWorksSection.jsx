b31aad1a313568882279c94974a9db7d1b0bfdb3
unknown <hamad200714@gmail.com>
2023-03-11 01:15:51 +0300

    new: temp  my works slider

--- src/sections/MyWorksSection.jsx
+++ src/sections/MyWorksSection.jsx
@@ -14,88 +14,117 @@ import { MdKeyboardDoubleArrowRight, MdKeyboardDoubleArrowLeft } from 'react-ico
 
 function MyWorksSection() {
     return (
-        <div className="py-10">
+        <div className="pt-10">
             <h2 className="title ml-6 mb-3">Some projects I worked on</h2>
+            <div className="slider-controler">
+                <div className="swiper-button-prev slider-arrow">
+                    <MdKeyboardDoubleArrowLeft />
+                </div>
+                <div className="swiper-button-next slider-arrow">
+                    <MdKeyboardDoubleArrowRight />
+                </div>
+            </div>
             <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                loop={true}
-               
-                slidesPerView={'auto'}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 2.5,
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
                 }}
-                pagination={{ el: '.swiper-pagination', clickable: true }}
+                grabCursor={true}
+                loop={true}
+                spaceBetween={20}
+                slidesOffsetBefore={20}
+                slidesPerView={1}
                 navigation={{
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
                     clickable: true,
                 }}
-                modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+                modules={[Pagination, Navigation, Autoplay]}
+                speed={700}
+                breakpoints={{
+                    640: {
+                      slidesPerView: 2,
+                      spaceBetween: 20,
+                    },
+                    768: {
+                      slidesPerView: 3,
+                      spaceBetween: 40,
+                    }
+                  }}
                 className="swiper_container"
             >
                 <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=1" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=2" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=3" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=4" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=5" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=6" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Tilt options={{ scale: 2, max: 25 }}>
-                        <div className="img-container">
-                            <img src="https://picsum.photos/200/300?random=7" />
-                        </div>
-                    </Tilt>
-                </SwiperSlide>
 
-                <div className="slider-controler">
-                    <div className="swiper-button-prev slider-arrow">
-                        <MdKeyboardDoubleArrowLeft/>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=1" />
                     </div>
-                    <div className="swiper-button-next slider-arrow">
-                        <MdKeyboardDoubleArrowRight/>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=2" />
                     </div>
-                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=3" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=4" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=5" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=6" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="img-container">
+                        <div className="overlay">
+                            <h3 className='font-bold'>Project title</h3>
+                            <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut harum eum architecto quidem quae rerum assumenda est tempora odit magnam adipisci enim ex dolorem blanditiis, nostrum repellat ab cupiditate!</p>
+                            <a href='#' className="">Visit website</a>
+                        </div>
+                        <img src="https://picsum.photos/200/300?random=7" />
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </div>
     )
